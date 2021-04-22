6e48cacda6bb5cce3d65de4dbe03a2d2721ccaaa
Jason <jasonjuenger@gmail.com>
2021-04-22 10:54:38 -0700

    add routes to authenticate existing user and create new user

--- animal_adoption/routes.py
+++ animal_adoption/routes.py
@@ -1,5 +1,5 @@
 from animal_adoption import app, User
-from flask import jsonify
+from flask import jsonify, request
 
 
 @app.route('/', endpoint='', methods=['GET'])
@@ -12,15 +12,56 @@ def login():
     return hello
 
 
-@app.route('/test-create-user', endpoint='test_create_user', methods=['GET'])
+@app.route('/create-user', endpoint='create-user', methods=['POST'])
 def create_user():
     """
-    Test user creation and password hashing
+    Create a new user with the provided credentials
     :return:
     """
-    new_user = User()
-    new_user.create_user('my_test', 'test123')
-    print(new_user.authenticate_user('my_test', 'test123'))
-    print(new_user.authenticate_user('my_test', 'test122'))
+    if not request.is_json:
+        print('uri=/login error="Missing JSON in request"')
+        return jsonify({"msg": "Missing JSON in request"}), 400
 
-    return jsonify(message='success'), 200
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if not username:
+        print('uri=/login error="Missing username parameter"')
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        print('uri=/login error="Missing password parameter"')
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    new_user = User()
+    result = new_user.create_user(username=username, password=password)
+    if result:
+        return jsonify(message='User {} creation successful'.format(username)), 200
+    else:
+        return jsonify(message='User {} creation failed'.format(username)), 500
+
+
+@app.route('/login', endpoint='login', methods=['POST'])
+def login():
+    """
+    Authenticate existing user
+    :return:
+    """
+    if not request.is_json:
+        print('uri=/login error="Missing JSON in request"')
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if not username:
+        print('uri=/login error="Missing username parameter"')
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        print('uri=/login error="Missing password parameter"')
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    result = User.authenticate_user(username=username, password=password)
+    if result:
+        return jsonify(message='User {} authentication successful'.format(username)), 200
+    else:
+        return jsonify(message='Bad username or password'), 401
